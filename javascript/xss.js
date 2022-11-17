57c4c323f0bb216cb2828e38161848e959b5679c
yushink <ysd.kang@gmail.com>
2022-11-17 22:00:38 +1100

    remove 3 js vulns

--- javascript/xss.js
+++ javascript/xss.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const router = express.Router()
+// const express = require('express')
+// const router = express.Router()
 
-router.get('/greeting', (req, res) => {
-    const { name }  = req.query;
-    res.send('<h1> Hello :'+ name +"</h1>")
-})
+// router.get('/greeting', (req, res) => {
+//     const { name }  = req.query;
+//     res.send('<h1> Hello :'+ name +"</h1>")
+// })
 
-router.get('/greet-template', (req,res) => {
-    name = req.query.name
-    res.render('index', { user_name: name});
-})
+// router.get('/greet-template', (req,res) => {
+//     name = req.query.name
+//     res.render('index', { user_name: name});
+// })
 
-module.exports = router
+// module.exports = router
